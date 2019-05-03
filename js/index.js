150c706710aa40f4879cfd854601069250d098c9
Vadim Brusinsky <39990941+brusx88@users.noreply.github.com>
2019-05-03 23:18:55 +0300

    Add files via upload
    second v

--- js/index.js
+++ js/index.js
@@ -20,11 +20,22 @@ function init() {
 
 //scroll
 
-$(document).ready(function(){
-    $("#nav").on("click","a", function (event) {
+$(document).ready(function () {
+    $("#nav").on("click", "a", function (event) {
         event.preventDefault();
-        var id  = $(this).attr('href'),
+        var id = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
+        $('body,html').animate({ scrollTop: top }, 1500);
     });
 });
+
+//yandex metrica
+
+// (function (m, e, t, r, i, k, a) { 
+//     m[i] = m[i] || function () { 
+//         (m[i].a = m[i].a || []).push(arguments) }; 
+//         m[i].l = 1 * new Date(); 
+//         k = e.createElement(t), a = e.getElementsByTagName(t)[0],
+//          k.async = 1, k.src = r, a.parentNode.insertBefore(k, a) })
+//          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); 
+// ym(53530384, "init", { clickmap: true, trackLinks: true, accurateTrackBounce: true, webvisor: true });
